73076c417c199684a781eb9c654b2c51ad923eac
IT21834424 <it21834424@my.sliit.lk>
2023-10-15 22:31:38 +0530

    file created

---
+++ FoodAndKitchen/index.js
@@ -0,0 +1,38 @@
+//const express = require ("express");
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from 'mongoose'
+import morgan from "morgan"
+import authRoutes from "./routes/auth.js";
+import categoryRoutes from "./routes/category.js";
+import productRoutes from "./routes/product.js";
+
+dotenv.config();
+
+//console.log("process =>" ,process);
+
+const app = express();
+
+//db
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>console.log("DB connected"))
+.catch((err)=>console.log("DB error=>",err));
+
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+//router middleware
+app.use("/api",authRoutes);
+app.use("/api",categoryRoutes);
+app.use("/api",productRoutes);
+
+
+const port= process.env.PORT || 8000;
+
+
+app.listen(port,()=>{
+console.log( `Node is running on port ${port}`);
+});
